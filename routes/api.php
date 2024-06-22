eeb6504ff2cb90c85bf703d65e760de380b2ee7f
Keattisak <OKeattisak.A@gmail.com>
2024-06-22 17:15:59 +0700

    Add Controller

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\ProvisionServer;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::resource('users', UserController::class);
+Route::resource('photos', PhotoController::class);
+Route::get('/server', ProvisionServer::class);
